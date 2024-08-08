baa021575c5c192a75d2145f9652a0dc3b8e3e6d
Tudor Alexe <71398994+alexet18@users.noreply.github.com>
2024-08-08 16:01:05 +0300

    github_final

--- src/routes/+page.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-    import DateAndLocation from "../lib/components/DateAndLocation.Svelte";
-    import Sponsors from "../lib/components/Sponsors.svelte";
-
-</script>
-
-
-<DateAndLocation></DateAndLocation>
-<Sponsors></Sponsors>
